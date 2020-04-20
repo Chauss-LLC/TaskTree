de74855b3b352157d8c4c0a279834b5ebcd4cd6a
KH9IZ <knyazevegor01@mail.ru>
2020-04-20 23:31:31 +0500

    Initial commit
    Signed-off-by: KH9IZ <knyazevegor01@mail.ru>

---
+++ src/main/kotlin/com/chauss/tasktree/app/MainApp.kt
@@ -0,0 +1,6 @@
+package com.chauss.tasktree.app
+
+import com.chauss.tasktree.view.MainView
+import tornadofx.*
+
+class MainApp : App(MainView::class)
---
+++ src/main/kotlin/com/chauss/tasktree/view/MainView.kt
@@ -0,0 +1,8 @@
+package com.chauss.tasktree.view
+
+import tornadofx.*
+
+class MainView : View() {
+    override val root = vbox {
+    }
+}
