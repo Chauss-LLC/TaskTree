a086218e0e3b6c628b43f3b6f368f4088d1600a2
KH9IZ <knyazevegor01@mail.ru>
2020-05-10 13:09:09 +0500

    Created test structure of tasks.
    Signed-off-by: KH9IZ <knyazevegor01@mail.ru>

--- src/main/kotlin/com/chauss/tasktree/view/MainView.kt
+++ src/main/kotlin/com/chauss/tasktree/view/MainView.kt
@@ -1,8 +1,15 @@
 package com.chauss.tasktree.view
 
+import com.chauss.tasktree.model.Task
 import tornadofx.*
 
 class MainView : View() {
-    override val root = vbox {
+    var rootTask = Task("MainTitle")
+    override val root = pane()
+
+    init {
+        val oneMore = Task("Second")
+        oneMore.children.addAll(Task("1"), Task("2"))
+        rootTask.children.addAll(Task("First"), oneMore, Task("Third"))
     }
 }
