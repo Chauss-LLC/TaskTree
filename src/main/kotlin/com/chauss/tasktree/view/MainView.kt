f8821869d07f24d7b112f098d5d3dcd2749b5eab
KH9IZ <knyazevegor01@mail.ru>
2020-05-10 13:15:09 +0500

    Realized drawing and dragging tasks
    Signed-off-by: KH9IZ <knyazevegor01@mail.ru>

--- src/main/kotlin/com/chauss/tasktree/view/MainView.kt
+++ src/main/kotlin/com/chauss/tasktree/view/MainView.kt
@@ -1,6 +1,7 @@
 package com.chauss.tasktree.view
 
 import com.chauss.tasktree.model.Task
+import javafx.scene.layout.Pane
 import tornadofx.*
 
 class MainView : View() {
@@ -11,5 +12,24 @@ class MainView : View() {
         val oneMore = Task("Second")
         oneMore.children.addAll(Task("1"), Task("2"))
         rootTask.children.addAll(Task("First"), oneMore, Task("Third"))
+
+        drawCurrent(rootTask, 0.0, 0.0)
+    }
+
+    private fun drawCurrent(task: Task, coordinateX: Double, coordinateY: Double): Pane {
+        val element = pane {
+            layoutX = coordinateX
+            layoutY = coordinateY
+            label(task.title)
+            setOnMouseDragged {
+                relocate(it.sceneX - this.width / 2, it.sceneY - this.height / 2)
+            }
+
+        }
+        root.add(element)
+        for ((i, child) in task.children.withIndex()) {
+            drawCurrent(child, coordinateX + i * 100.0, coordinateY + 100.0)
+        }
+        return element
     }
 }
