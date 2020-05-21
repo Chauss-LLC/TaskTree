4a457f9d9310fec08d777c7cbda09b3318accf41
ubah <sudorgin12@gmail.com>
2020-05-21 21:52:23 +0500

    My first commit (DOES IT WORK??)

--- src/main/kotlin/com/chauss/tasktree/app/MainApp.kt
+++ src/main/kotlin/com/chauss/tasktree/app/MainApp.kt
@@ -3,4 +3,6 @@ package com.chauss.tasktree.app
 import com.chauss.tasktree.view.MainView
 import tornadofx.*
 
-class MainApp : App(MainView::class)
+class MainApp : App(MainView::class){
+
+}
--- src/main/kotlin/com/chauss/tasktree/view/MainView.kt
+++ src/main/kotlin/com/chauss/tasktree/view/MainView.kt
@@ -1,23 +1,60 @@
 package com.chauss.tasktree.view
 
-import com.chauss.tasktree.model.Task
+import com.chauss.tasktree.model.*
 import javafx.scene.layout.Pane
 import tornadofx.*
 
 class MainView : View() {
     var rootTask = Task("MainTitle")
-    private val linesLayout = pane()
-    private val buttonsLayout = pane()
-    override val root = stackpane {
-        add(linesLayout)
-        add(buttonsLayout)
+    val model = TaskModel(rootTask)
+    var form = borderpane(){
+        isVisible = false
+        center {
+            form {
+                fieldset("Новая задача") {
+                    field("Краткое описание") {
+                        textfield(model.title)
+                    }
+                    fieldset("Дата") {
+                        datepicker(model.deadline)
+                    }
+                    fieldset("Подробное описание") {
+                        textfield(model.description)
+                    }
+                }
+            }
+        }
+        bottom{
+            hbox {
+                this.add(button("save") {
+                    hboxConstraints {
+                        marginRight = 200.0
+                    }
+                    action {
+                        save()
+                    }
+                })
+                this.add(button("close"){
+                    action{
+                        this@borderpane.isVisible = false
+                    }
+                })
+            }
+        }
     }
+    private fun save(){
+        model.commit()
+        val task = model.item
+        rootTask = task
+        println("SAVING ${rootTask.title} / ${rootTask.deadline} / ${rootTask.description}")
+    }
+
+    override val root = pane()
 
     init {
         val oneMore = Task("Second")
         oneMore.children.addAll(Task("1"), Task("2"))
         rootTask.children.addAll(Task("First"), oneMore, Task("Third"))
-
         drawCurrent(rootTask, 0.0, 0.0)
     }
 
@@ -25,17 +62,24 @@ class MainView : View() {
         val element = pane {
             layoutX = coordinateX
             layoutY = coordinateY
-            button(task.title) {
-                setOnMouseDragged {
-                    this@pane.relocate(it.sceneX - this.width / 2, it.sceneY - this.height / 2)
+            button{
+                this.text().bind(task.titleProperty)
+                action{
+                    form.layoutX = 350.0
+                    form.layoutY = 0.0
+                    form.isVisible = true
+                    add(form)
                 }
+            }
+            setOnMouseDragged {
+                    relocate(it.sceneX - this.width / 2, it.sceneY - this.height / 2)
             }
 
         }
-        buttonsLayout.add(element)
+        root.add(element)
         for ((i, child) in task.children.withIndex()) {
             val drewElement = drawCurrent(child, coordinateX + i * 100.0, coordinateY + 100.0)
-            with(linesLayout) {
+            with(root) {
                 line(coordinateX, coordinateY, coordinateX + i * 100, coordinateY + 100) {
                     startXProperty().bind(element.layoutXProperty())
                     startYProperty().bind(element.layoutYProperty())
@@ -46,4 +90,5 @@ class MainView : View() {
         }
         return element
     }
+
 }
